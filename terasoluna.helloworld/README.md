e3b143bc01d26521f0065fa194ab40c28808e413
hejin <hejin82@gmail.com>
2018-03-21 21:15:05 +0900

    add terasoluna.helloworld

---
+++ terasoluna.helloworld/README.md
@@ -0,0 +1,11 @@
+# terasoluna.helloworld
+
+```
+mvn archetype:generate -B^
+ -DarchetypeGroupId=org.terasoluna.gfw.blank^
+ -DarchetypeArtifactId=terasoluna-gfw-web-blank-archetype^
+ -DarchetypeVersion=5.4.1.RELEASE^
+ -DgroupId=org.hejin^
+ -DartifactId=terasoluna.helloworld^
+ -Dversion=1.0.0
+ ```
---
+++ terasoluna.helloworld/src/main/java/org/hejin/app/echo/EchoForm.java
@@ -0,0 +1,24 @@
+package org.hejin.app.echo;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class EchoForm implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull
+	@Size(min = 1, max = 5)
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
